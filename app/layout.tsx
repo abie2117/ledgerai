f84b18f048731fc0cbe02378e8f18c804335310c
abie2117 <benedictaijisesan@gmail.com>
2026-08-27 13:13:06 -0700

    Fix Route

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-slate-50 text-slate-900 antialiased">
+      <body className="bg-slate-50 text-slate-900 antialiased min-h-screen">
         {children}
       </body>
     </html>
